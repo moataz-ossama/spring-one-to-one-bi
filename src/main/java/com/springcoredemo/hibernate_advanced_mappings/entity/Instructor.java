22a0903f44eb3bc18b19d4135b8abddf12517f1d
moataz_ossama <moataz.ossama.a@gmail.com>
2024-12-08 02:45:27 +0200

    commit change 8

--- src/main/java/com/springcoredemo/hibernate_advanced_mappings/entity/Instructor.java
+++ src/main/java/com/springcoredemo/hibernate_advanced_mappings/entity/Instructor.java
@@ -12,11 +12,11 @@ public class Instructor {
    @GeneratedValue (strategy = GenerationType.IDENTITY)
    @Column(name = "id")
 private int id;
-   @Column(name = "first_name_1")
+   @Column(name = "first_name")
 private String firstName;
-   @Column(name = "last_name_2")
+   @Column(name = "last_name_1111")
 private String lastName;
-   @Column(name = "email_0")
+   @Column(name = "email_111")
 private String email;
    @OneToOne(cascade = CascadeType.ALL)
    @JoinColumn(name = "instructor_detail_id")
